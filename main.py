4bd828cabe01b0343f63a23e6317a457f422782b
sonya <122512138+werxqq0@users.noreply.github.com>
2023-09-10 17:59:20 +0200

    Update 1.0.2
    - Code improvement
    - Text upgrades

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from bs4 import BeautifulSoup
 
 download_folder = 'samplefocus_export'
 
-print('SampleFocus Unlocked 1.0.1\nMade by werxqq0\nhttps://github.com/werxqq0/SampleFocus-Unlocked\n')
+print('SampleFocus Unlocked 1.0.2\nMade by werxqq0\nhttps://github.com/werxqq0/SampleFocus-Unlocked\n')
 
 while True:
-    music_link = input('Music Link: ')
+    input_audio_link = input('Music Link: ')
 
     def download_music(link):
         if link.startswith('https://samplefocus.com/'):
@@ -25,29 +25,29 @@ while True:
 
                         props_json = json.loads(data_react_props)
 
-                        music_url = props_json.get('sample').get('sample_mp3_url')
+                        audio_link = props_json.get('sample').get('sample_mp3_url')
 
-                        if music_url:
-                            file_name = os.path.basename(f'{music_link}.mp3')
+                        if audio_link:
+                            file_name = os.path.basename(f'{input_audio_link}.mp3')
                             file_path = os.path.join(download_folder, file_name)
 
-                            response = requests.get(music_url)
+                            response = requests.get(audio_link)
                             if response.status_code == 200:
                                 with open(file_path, 'wb') as file:
                                     file.write(response.content)
-                                print(f'File {file_name} downloaded successfully.')
+                                print(f'File {file_name} downloaded successfully.\n')
                             else:
-                                print(f'Cannot download file {file_name}!')
+                                print(f'Cannot download file {file_name}!\n')
                         else:
-                            print('Could not find a link to download music on the page!')
+                            print('Could not find a link to download music on the page!\n')
                     else:
-                        print('The download element could not be found on the page!')
+                        print('The download element could not be found on the page!\n')
                 else:
-                    print('This audio does not exist!')
+                    print('This audio does not exist!\n')
 
-            download_system(music_link)
+            download_system(input_audio_link)
         else:
-            print('This link is not a SampleFocus site!')
+            print('This link is not a SampleFocus site!\n')
             return False
 
-    download_music(music_link)
+    download_music(input_audio_link)
